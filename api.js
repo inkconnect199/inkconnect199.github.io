9b75a66232a03362bb713de02336c1780385d081
inkconnect199 <inkconnect661@gmail.com>
2026-09-25 09:58:39 +0300

    Update GAS_URL in API_CONFIG

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ const API_CONFIG = {
   // Paste your deployed Google Apps Script Web App URL here, e.g.
   // "https://script.google.com/macros/s/AKfycb.../exec"
   GAS_URL: "https://script.google.com/macros/s/AKfycbwe9e3u1eG6WpVp4UcqOOrDNK1234HPzqcRFhGZZ4muwKK_3Vw4N20AnoB-qityi-8R3Q/exec",
-};
+
 
 const DB_KEY = "inkconnect_db_v1";
 const SESSION_KEY = "inkconnect_session_v1";
